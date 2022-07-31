15cf62ea80d0cc565c109b4ea103af84b2a29ebb
CyberYoshi64 <cyb3ryoshi64@gmail.com>
2022-07-31 21:30:09 +0200

    Add save data backup to terminal version

--- terminalMain.py
+++ terminalMain.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from CTGP7UpdaterApp.CTGP7Updater import CTGP7Updater
-import os, argparse
+import os, shutil, argparse
 
 def logger(data:dict):
     if "m" in data:
@@ -13,18 +13,19 @@ def logger(data:dict):
 
 def ConsoleConfirmed(message:str):
     print(message)
-    try:    return input("[yN]").upper()=="Y"
+    try:    return input("[y/N]").upper()=="Y"
     except: return False
 
 argprs = argparse.ArgumentParser()
-argprs.add_argument("-i", "--install", action="store_true", help="Force a re-install, even is updates are viable.")
-argprs.add_argument("-p", dest="path", help="Specify a path to a 3DS SD Card instead of auto-detecting.")
+argprs.add_argument("-i", "--install", action="store_true", help="Force a re-install, even is updating is viable.")
+argprs.add_argument("-p", dest="path", help="Specify a path to a 3DS SD Card instead of trying to auto-detect one.")
 arg = argprs.parse_args()
 try:
     _INSTALLER_VERB = {True: "install", False: "update"}
     makeNewInstall = False
+    madeSaveBackup = False
     
-    print("CTGP-7 Update Tool v1.2")
+    print("CTGP-7 Update Tool (Terminal) v1.2")
     if arg.path != None:
         sdPath = arg.path
         print("Using path from argument")
@@ -32,7 +33,7 @@ try:
         sdPath = CTGP7Updater.findNintendo3DSRoot()
         if sdPath == None:
             raise Exception("Cannot determine a suitable SD Card.")
-        print("Using path: \"{}\"".format(sdPath))
+        print("Detected SD Card: \"{}\"".format(sdPath))
     
     if not os.path.exists(sdPath):
         raise Exception("The path '{}' does not exist. Make sure you entered the path correctly.".format(sdPath))
@@ -40,13 +41,15 @@ try:
     not ConsoleConfirmed("""\
 This path does not appear to be that of a SD Card
 from a Nintendo 3DS system.
-
 Path chosen: {}
 
 Continue anyway?""".format(sdPath)):
         raise Exception("User cancelled installation.")
 
     installPathFlag = CTGP7Updater.checkForInstallOfPath(sdPath)
+    savefsPath = os.path.join(sdPath, "CTGP-7", "savefs")
+    savefsCopy = os.path.join(sdPath, "CTGP-7savebak")
+
     if installPathFlag & 3:
         if not ConsoleConfirmed("""\
 There was no valid CTGP-7 installation detected.
@@ -60,6 +63,7 @@ Do you wish to continue anyway?"""):
     # Console-only; GUI will warn immediately and not take a hold
     # of this value
     if makeNewInstall: installPathFlag &= 3
+
     updater = CTGP7Updater(makeNewInstall)
     updater.fetchDefaultCDNURL()
     updater.setLogFunction(logger)
@@ -69,7 +73,7 @@ Do you wish to continue anyway?"""):
     updater.verifySpaceAvailable()
 
     # This question is only for console; for GUI, take the value
-    # of installPathFlag and check to verfiy entered path.
+    # of installPathFlag and check to verify the entered path.
     # Console is one-shot.
     if (installPathFlag & 4):
         if not ConsoleConfirmed("""\
@@ -83,24 +87,48 @@ Proceeding to {}ing to version '{}'.
 Do you want to proceed?""".format(_INSTALLER_VERB[updater.isInstaller], updater.latestVersion)):
             raise Exception("User cancelled the installation.")
 
+    if makeNewInstall:
+        if os.path.exists(savefsPath):
+            try:
+                shutil.copytree(savefsPath, savefsCopy)
+                madeSaveBackup = True
+                print("Your current CTGP-7 save data was backed up in this directory:\n{}".format(savefsCopy))
+            except Exception as e:
+                if not ConsoleConfirmed("""\
+Failed making backup of the save data:
+{}
+
+Proceeding will wipe your save data. Continue?""".format(e)):
+                    raise Exception("User cancelled installation: save backup failed")
+
     updater.cleanInstallFolder()
     updater.startUpdate()
+
+    if madeSaveBackup:
+        if ConsoleConfirmed("Do you want to restore the save backup done previously?"):
+            try:
+                shutil.copytree(savefsCopy,savefsPath)
+            except:
+                print("""\
+Failed restoring the backup.
+You need to do it manually. Copy from:
+{}
+To:
+{}
+""".format(savefsCopy, savefsPath))
 
     print("""
 The installation of version {} was successful.
 
 Make sure to install the following CIA using FBI:
-SD > CTGP-7 > cia > CTGP-7.cia
-""".format(updater.latestVersion))
+  SD > CTGP-7 > cia > CTGP-7.cia""".format(updater.latestVersion))
 except Exception as e:
     print("""
 An error has occured:
 {}
 
-Should issues persist, ask for help in the
-CTGP-7 Discord Server.
-Invite: https://discord.com/invite/0uTPwYv3SPQww54l
-""".format(e))
+Should issues persist, ask for help in the CTGP-7
+Discord Server: https://discord.com/invite/0uTPwYv3SPQww54l""".format(e))
 
 try:    input("Press Return to exit the application.")
 except: pass
